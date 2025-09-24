e30423b67593c0441acefb5faa594dbb9b98c58d
Richard2111 <richardandrade2111@gmail.com>
2025-09-24 12:02:15 -0400

    Prueba 9

--- components/TicketForm.js
+++ components/TicketForm.js
@@ -280,9 +280,11 @@ export default function TicketForm() {
             id="reference"
             value={reference}
             onChange={(e) => setReference(e.target.value.replace(/\D/g, ''))} // Solo permite dígitos
+            minLength={6} // Mínimo 6 caracteres
             maxLength="6"
             className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 font-medium text-gray-900"
             required
+            pattern="\d{6}"
           />
         </div>
 
